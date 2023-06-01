fa7d764c86e5c34e29b00863ff93dda855052e49
luciorq <luciorqueiroz@gmail.com>
2023-06-01 13:09:30 -0400

    feat: create gcp-hpc project

---
+++ gcp-hpc/variables.tf
@@ -0,0 +1,13 @@
+variable "gcp_region" {
+  description = "GCP region for all resources."
+
+  type = string
+  default = "us-central1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone for all resources."
+
+  type = string
+  default = "us-central1-b"
+}
